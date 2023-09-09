952da247ee906b54be34fab8438816854655a039
hsingla378 <37644800+hsingla378@users.noreply.github.com>
2023-09-09 13:39:52 +0530

    add header

---
+++ frontend/src/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import Hero from "./Hero/Hero";
+import Footer from "../Footer/Footer";
+
+export default function LandingPage(){
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  );
+};
